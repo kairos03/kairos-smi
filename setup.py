7615e362090c42897b1e40e22dd4c886ea66a4ac
kairos <kairos9603@gmail.com>
2019-04-09 13:34:44 +0900

    fix packaging config files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='kairos-smi',
+    name='ksmi',
     version='0.1.0',
     url='https://github.com/kairos03/kairos-smi',
     license='MIT',
@@ -10,7 +10,13 @@ setup(
     description='Multi-server GPU monotoring tools',
     packages=find_packages(exclude=['tests', 'config.json']),
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     zip_safe=False,
     setup_requires=['nose>=1.0'],
-    test_suite='nose.collector'
+    test_suite='nose.collector',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
