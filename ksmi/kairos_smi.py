576cea6bec3a4df32cd7708ba43eacc2d6b848d7
doublejtoh <doublejtoh@naver.com>
2019-08-30 14:47:35 +0900

    ksmi process option updated

--- ksmi/kairos_smi.py
+++ ksmi/kairos_smi.py
@@ -16,19 +16,23 @@ logging.basicConfig(level=logging.ERROR)
 
 # querys
 QUERY_GPU = "nvidia-smi --query-gpu=timestamp,gpu_uuid,count,name,pstate,temperature.gpu,utilization.gpu,memory.used,memory.total --format=csv,noheader"
-QUERY_APP = "nvidia-smi --query-compute-apps=gpu_uuid,pid,process_name,used_memory --format=csv,noheader"
+QUERY_APP = """
+        a=($(nvidia-smi --query-compute-apps=gpu_uuid,pid,used_memory --format=csv,noheader,nounits | awk '{print $1,$2,$3}' FS=', ' OFS=','))
+        for item in $a
+        do
+            pid=$(echo $item | awk '{print $2}' FS=',') 
+            ps=$(ps --noheader -o "pid,user,%cpu,%mem,etime,command" -p $pid)
+            echo $item | awk '{printf "%s, %s, ", $1, $3}' FS="," RS="\n" 
+            echo $ps | awk 'BEGIN {OFS=", "} {$1=$1; print}' RS="\n"
+        done;
+        """
 
-# process detail query interval
-APP_DETAIL_QUERY_INTERVAL = 10
-
+#QUERY_APP = "nvidia-smi --query-compute-apps=gpu_uuid,pid,process_name,used_memory --format=csv,noheader"
 
 def ssh_remote_command(entrypoint, command, timeout=1):
 
+    def postprocessing(data):
         return [x.split(', ') for x in data.decode('utf-8').split('\n')[:-1]]
-    elif type == 'ps':
-        return data.decode('utf-8').split('\n')[:-1]
-
-def ssh_remote_command(entrypoint, command, type='ssh'):
 
     try:
         host, port = entrypoint.split(':')
@@ -40,7 +44,6 @@ def ssh_remote_command(entrypoint, command, type='ssh'):
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE)
     try:
-
         out, err = ssh.communicate(timeout=timeout)
         #print(out, err)
         if err != b'':
@@ -99,17 +102,12 @@ def get_gpus_status(hosts, timeout=1):
 def display_gpu_status(hosts, data):
     """Display gpu status
     """
-
-
-    def run_command_query_process_details(q, host, query):
-        result = ssh_remote_command(host, query, 'ps')
-        q.put(result)
-
     for host in hosts:
         gpu_stat = data[host].get('gpus')
         app_stat = data[host].get('apps')
         active_gpus = len(set(app_info[0] for app_info in app_stat))
         
+        # print gpu stat
         # if gpu stat is empty
         print('[{:.30}]'.format(host), end='')
         if gpu_stat == None or app_stat == None or len(gpu_stat) == 0:
@@ -123,24 +121,12 @@ def display_gpu_status(hosts, data):
             if len(gpu) != 9:
                 continue
             print("| {} | Temp {:2s}C | Util {:>5s} | Mem {:>6s} / {:9s} |".format(i, gpu[5], gpu[6], gpu[7][:-4], gpu[8]))
-            nvidia_app_infos = []
-            ps_infos = Queue(maxsize=100)
-            
-            # for fast search 
-            if True:
-                if gpu[1] in app_data[host].keys():
-                    for i, app in enumerate(app_data[host][gpu[1]]):
-                        if i == len(app_data[host][gpu[1]]) - 1:
-                            print("\t└── process PID {:s} | Username {:s} | used_memory {:s} ".format(app[0], app[1], app[2]))
-                        else:
-                            print("\t├── process PID {:s} | Username {:s} | used_memory {:s} ".format(app[0], app[1], app[2]))
-                    
-                        
             
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', default='config.json', help='set config file location')
+    parser.add_argument('-p', '--process', action='store_true', help='show process details (e.g. username, used_memory, cpu util, execution time, command)')
     args = parser.parse_args()
     return args
 
@@ -155,12 +141,11 @@ def main():
         exit()
 
     HOSTS = conf['hosts']
-    APP_DETAIL_QUERY_INTERVAL = 10
 
     # init screen
     screen = ui.init_screen()
     while(True):
-        result = get_gpus_status(HOSTS)
+        result = get_gpus_status(HOSTS, timeout=2)
 
         key = screen.getch()
         if key == ord('q'):
@@ -168,8 +153,9 @@ def main():
             break
 
         logging.debug("result {}".format(result))
+
         try:
-            ui.display(screen, HOSTS, result)
+            ui.display(screen, HOSTS, result, args.process)
         except curses.error:
             pass
 
