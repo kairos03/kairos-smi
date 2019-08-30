25772dd4adc7b6545827b05d59decbf5ccec8f66
kairos03 <kairos9603@gmail.com>
2019-08-30 18:42:11 +0900

    chagne test code to use mock

--- tests/test_get_gpu_status.py
+++ tests/test_get_gpu_status.py
@@ -5,17 +5,29 @@ from ksmi.kairos_smi import get_gpus_status
 class test_get_gpu_status(unittest.TestCase):
     
     def setUp(self):
-        self.hosts = ["mlvc07@163.180.186.49:2222"]
-        self.wrong_hosts = ["test@123.123.123.123:2211"]
+        self.success_hosts = ["success@111.111.111.111:22"]
+        self.error_hosts = ["error@123.123.123.123:2211"]
         self.timeout = 10
+        self.success_return = {
+            "success@111.111.111.111:22":{
+                "gpus":[['2019/08/30 18:36:43.366', 'GPU-744c553a-2cfe-636a-9a49-ef88a4de14f4', '4', 'GeForce GTX 1080 Ti', 'P8', '29', '0 %', '1 MiB', '11178 MiB'], 
+                        ['2019/08/30 18:36:43.370', 'GPU-3c7715ab-2204-60a3-bf01-accb2360644f', '4', 'GeForce GTX 1080 Ti', 'P2', '62', '100 %', '4347 MiB', '11178 MiB'], 
+                        ['2019/08/30 18:36:43.372', 'GPU-94405968-473a-6e5f-4002-ba9e6243091d', '4', 'GeForce GTX 1080 Ti', 'P2', '62', '99 %', '4421 MiB', '11178 MiB'], 
+                        ['2019/08/30 18:36:43.387', 'GPU-2fd0b93b-1d0f-cce9-c282-fdc43ab5eeb0', '4', 'GeForce GTX 1080 Ti', 'P8', '39', '0 %', '1 MiB', '11175 MiB']], 
+                'apps': [['GPU-3c7715ab-2204-60a3-bf01-accb2360644f', '2275', '24827', 'youmin', '99.3', '3.8', '04:25:31', 'python3', './train.py', '--type', 'cifar100', '--model', 'resnet56', '--augtype', 'puzzle6_4', '--tn', 'ox_100_2'], 
+                        ['GPU-3c7715ab-2204-60a3-bf01-accb2360644f', '2061', '5753', 'mlvcgpu', '99.1', '4.2', '03:14:16', 'python3', 'train_CIFAR10.py', '-act', 'PRELU3', '-net', 'resnet', '-resume', 'no'], 
+                        ['GPU-94405968-473a-6e5f-4002-ba9e6243091d', '2275', '8736', 'youmin', '99.2', '3.8', '04:25:03', 'python3', './train.py', '--type', 'cifar100', '--model', 'resnet56', '--augtype', 'puzzle6_4', '--tn', 'ox_100_3'], 
+                        ['GPU-94405968-473a-6e5f-4002-ba9e6243091d', '2135', '25653', 'mlvcgpu', '99.3', '4.2', '02:49:49', 'python3', 'train_CIFAR10.py', '-act', 'PRELU4', '-error', '0.09', '-net', 'resnet']]
+            }
+        }
 
     def test_get_gpu_status_success(self):
         # success case
-        results = get_gpus_status(self.hosts, self.timeout)
+        results = get_gpus_status(self.success_hosts, self.timeout)
         print(results)
         self.assertEqual(type(results), type({}))
         self.assertEqual(len(results), 1)
-        self.assertTrue(self.hosts[0] in results.keys())
+        self.assertTrue(self.success_hosts[0] in results.keys())
         for entry in results.keys():
             self.assertEqual(type(results[entry]), type({}))
             self.assertEqual(len(results[entry]), 2)
@@ -27,10 +39,10 @@ class test_get_gpu_status(unittest.TestCase):
 
     def test_get_gpu_status_fail(self):
         # fail case
-        results = get_gpus_status(self.wrong_hosts, self.timeout)
+        results = get_gpus_status(self.error_hosts, self.timeout)
         self.assertEqual(type(results), type({}))
         self.assertEqual(len(results), 1)
-        self.assertTrue(self.wrong_hosts[0] in results.keys())
+        self.assertTrue(self.error_hosts[0] in results.keys())
         for entry in results.keys():
             #print(results)
             self.assertEqual(type(results[entry]), type({}))
@@ -41,9 +53,7 @@ class test_get_gpu_status(unittest.TestCase):
             self.assertTrue(len(results[entry]['apps']) == 0)
             #print(results[entry]['apps'])
 
-    # def test_display_gpu_status(self):
-    #     result = get_gpus_status(self.hosts)
-    #     display_gpu_status(self.hosts, result)
-
-    #def test_main(self):
-    #    main()
+    @unittest.skip("skip when unittest")
+    def test_real(self):
+        results = get_gpus_status(["mlvc03@mlvc03:2222"], self.timeout)
+        print(results)
--- tests/test_ssh_remote_command.py
+++ tests/test_ssh_remote_command.py
@@ -7,13 +7,12 @@ from ksmi.kairos_smi import ssh_remote_command, QUERY_APP, QUERY_GPU
 class test_ssh_remote_command(unittest.TestCase):
     
     def setUp(self):
-        self.host = "mlvc07@163.180.186.49:2222"
-        self.wrong_host = "test@123.123.123.123:2211"
+        self.success_host = "success@111.111.111.111:22"
+        self.error_host = "error@123.123.123.123:2211"
         self.timeout = 1
-        self.return_ssh_success = (b"""
-            2019/08/30 12:04:44.462, GPU-e831358e-b0e6-38c1-eea8-4f08d58ebe5d, 7, GeForce RTX 2080 Ti, P2, 70, 81 %, 10256 MiB, 10989 MiB\n
-            2019/08/30 12:04:44.472, GPU-25ca9c48-5004-6d7a-57ba-f8fce126957c, 7, GeForce RTX 2080 Ti, P8, 29, 0 %, 11 MiB, 10989 MiB\n
-            """,
+        self.return_ssh_success = (
+            b'2019/08/30 12:04:44.462, GPU-e831358e-b0e6-38c1-eea8-4f08d58ebe5d, 7, GeForce RTX 2080 Ti, P2, 70, 81 %, 10256 MiB, 10989 MiB\n'
+            + b'2019/08/30 12:04:44.472, GPU-25ca9c48-5004-6d7a-57ba-f8fce126957c, 7, GeForce RTX 2080 Ti, P8, 29, 0 %, 11 MiB, 10989 MiB\n',
             b'')
         self.return_ssh_error = (b'', b'Error')
         self.return_ssh_timeout = (b'', b'Timeout')
@@ -21,42 +20,29 @@ class test_ssh_remote_command(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # TODO skip
-    def test_echo_success(self):
+    @patch('subprocess.Popen.communicate')
+    def test_echo_success(self, mock_subprocess):
+        mock_subprocess.return_value = (b'hello\nhi\n', b'')
         # success case
-        result = ssh_remote_command(self.host, 'echo hello; echo hi', self.timeout)
+        command = 'echo hello; echo hi'
+        result = ssh_remote_command(self.success_host, command, self.timeout)
         self.assertEqual(result,
             {
                 'status': 'Success',
-                'entry': self.host, 
-                'command': 'echo hello; echo hi', 
+                'entry': self.success_host, 
+                'command': command, 
                 'data': [['hello'], ['hi']]
             })
     
-    @patch('subprocess.Popen')
-    def test_query_success(self, mock_Popen):
-        mock_Popen.communicate.side_effect = self.return_ssh_success
+    @patch('subprocess.Popen.communicate')
+    def test_query_success(self, mock_subprocess):
+        mock_subprocess.return_value = self.return_ssh_success
         # success case
-        result = ssh_remote_command(self.host, QUERY_GPU, self.timeout)
+        result = ssh_remote_command(self.success_host, QUERY_GPU, self.timeout)
         self.assertEqual(result['status'], 'Success')
+        print(result['data'])
         
     def test_query_fail(self):
         # fail case
-        result = ssh_remote_command(self.wrong_host, 'echo hello', self.timeout)
+        result = ssh_remote_command(self.error_host, 'echo hello', self.timeout)
         self.assertEqual(result['status'], 'Timeout')
-
-    def test_query_process(self):
-        query = """
-        a=($(nvidia-smi --query-compute-apps=gpu_uuid,pid,used_memory --format=csv,noheader,nounits | awk '{print $1,$2,$3}' FS=', ' OFS=','))
-        for item in $a
-        do
-            ps=$(ps --noheader -o "pid,user,%cpu,%mem,etime,command" -p $(echo $item | awk '{print $2}' FS=','))
-            echo $item | awk '{printf "%s, %s, ", $1, $3}' FS="," RS="\n" 
-            echo $ps | awk 'BEGIN {OFS=", "} {$1=$1; print}' RS="\n"
-        done;
-        """
-        
-        result = ssh_remote_command(self.host, query, 2)
-        print(result['data'])
-        result = ssh_remote_command(self.host, 'echo test', 1)
-        print(result['data'])
